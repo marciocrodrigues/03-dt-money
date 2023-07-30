16bee26c9e86bd4937c8158102f03158615c9f14
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-30 00:42:03 -0300

    inicio desenvolvimento componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
+
 export function App() {
   return (
-    <>
-      Hello World
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
   )
 }
